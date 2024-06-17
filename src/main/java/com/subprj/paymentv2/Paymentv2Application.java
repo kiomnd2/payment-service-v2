5b968fb82a3330f11512595a1abca7e09f2c4c78
kiomnd2 <kiomnd2@gmail.com>
2024-06-17 20:09:07 +0900

    first commit

---
+++ src/main/java/com/subprj/paymentv2/Paymentv2Application.java
@@ -0,0 +1,13 @@
+package com.subprj.paymentv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paymentv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paymentv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/subprj/paymentv2/Paymentv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subprj.paymentv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paymentv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
